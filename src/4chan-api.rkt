46b7f44800d9e87b9d87577af5860302acd67ae6
Dexter Haslem <dexter.haslem@gmail.com>
2014-06-14 20:04:50 -0600

    keep board ref in structs
    this allows ownership lookup which will be needed on posts for
    construction of final file url

--- src/4chan-api.rkt
+++ src/4chan-api.rkt
@@ -36,9 +36,9 @@
   (get-boards-list (hash-ref [get-boards-json] 'boards)))
 
 ; number, subject, comment, file ext, file timestamp (renamed name), replies count, imgs# count
-(struct catalog-threadinfo (no sub com ext tim replies# imgs#) #:transparent)
+(struct catalog-threadinfo (no sub com ext tim replies# imgs# owner-board) #:transparent)
 
-(define (catalog-thread-json->catalog-threadinfo hash)
+(define (catalog-thread-json->catalog-threadinfo hash board)
   (catalog-threadinfo 
    (if (hash-has-key? hash 'no) (hash-ref hash 'no) 0)
    (if (hash-has-key? hash 'sub) (hash-ref hash 'sub) "")
@@ -46,11 +46,12 @@
    (if (hash-has-key? hash 'ext) (hash-ref hash 'ext) "")
    (if (hash-has-key? hash 'tim) (hash-ref hash 'tim) "")
    (if (hash-has-key? hash 'replies) (hash-ref hash 'replies) "")
-   (if (hash-has-key? hash 'images) (hash-ref hash 'images) "")))
+   (if (hash-has-key? hash 'images) (hash-ref hash 'images) ""))
+  board)
 
-(define (parse-page-threadinfo thread-hash)
+(define (parse-page-threadinfo thread-hash owner-board)
   (if (hash? thread-hash)
-      (catalog-thread-json->catalog-threadinfo thread-hash)
+      (catalog-thread-json->catalog-threadinfo thread-hash owner-board)
       'bad-thread))
 
 (define (get-catalog-json board) 
@@ -62,7 +63,8 @@
   ; a list of pages, and each page has a list of threads (all hashes)
   (define (get-page-threads page-json)
     (define threads [hash-ref page-json 'threads])
-    (map parse-page-threadinfo threads))
+    (map (lambda (t) 
+           (parse-page-threadinfo threads board))))
   
   (define (get-all-page-threads lst)
     (cond
@@ -78,10 +80,10 @@
 (define (get-board-threadlist-json board)
   (get-url-json (format "http://a.4cdn.org/~a/threads.json" [board-name board])))
 
-(struct board-thread (no lastmodified) #:transparent)
+(struct board-thread (no lastmodified board) #:transparent)
 
 (define (board-thread->string b)
-   (format "board no#=~a last_modified=~a" [board-thread-no b] [board-thread-lastmodified b]))
+   (format "thread no#=~a last_modified=~a" [board-thread-no b] [board-thread-lastmodified b]))
 
 (define (get-board-threads board)
   (define board-thread-json (get-board-threadlist-json board))
@@ -97,11 +99,42 @@
            [(empty? thread-list) empty]
            [else 
             (define cur-thread [first thread-list])
-            (cons [board-thread (hash-ref cur-thread 'no) (hash-ref cur-thread 'last_modified)]
+            (cons [board-thread (hash-ref cur-thread 'no) (hash-ref cur-thread 'last_modified) board]
                   [walk-threads (rest thread-list)])]))
        (cons (walk-threads threads) (parse-pages (rest lst)))]))
   (flatten (parse-pages board-thread-json)))
 
-(define (test-get-board-threads)
-  (get-board-threads (first (get-boards))))
+;(define (test-get-board-threads)
+;  (get-board-threads (first (get-boards))))
+
+#| thread 'post' code - a thread is just a list of posts
+   each post section looks something like this:
+{
+            "no": 6326151,
+            "now": "06/14/14(Sat)17:22:51",
+            "name": "Anonymous",
+            "filename": "tumblr_n73jm0d4nH1tw0p0po1_500",
+            "ext": ".gif",
+            "w": 500,
+            "h": 250,
+            "tn_w": 125,
+            "tn_h": 62,
+            "tim": 1402780971833,
+            "time": 1402780971,
+            "md5": "pr8H7r7R8im6jenRuu+wSw==",
+            "fsize": 1098257,
+            "resto": 6326147
+        },
+thread json is a posts element with list of .. posts.
+|#
+
+; the post struct only contains things we care about - details to grab the file
+(struct post (no ext tim fsize))
+
+(define (get-thread-json board thread-no#)
+  (get-url-json (format "http://a.4cdn.org/~a/thread/~a.json" [board-name board] thread-no#)))
+
+(define (get-thread-posts thread)
+  (printf "todo~n"))
+
     
